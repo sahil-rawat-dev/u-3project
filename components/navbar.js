90c6f0e60211123308de879a825bf280381c6335
Rahul Baghel <rahulbaghel6211@gmail.com>
2022-02-24 11:55:24 +0530

    nav commit

---
+++ components/navbar.js
@@ -0,0 +1,39 @@
+function navbar(){
+    return`<div id="navbar1">
+    <div id="nav1">
+        <button id="Shipping"><i id="fa1" class="fa-solid fa-gift"></i>myGlamm Superfoods Cacao From Africa</button>
+        <button id="collect">SHOP NOW</button>
+
+    </div>
+    
+    <div id="nav2">
+    <button class="icon"><i id="fas1" class="fas fa-store"></i>MyGlamm Store</button>
+</div>
+</div>
+<div id="navbar123">
+    <img id="img1" src="https://www.investindia.gov.in/themes/investindia/image/unicorns/2021/MyGlamm.png" alt="">
+    <div class="search1">
+        <div class="search2"><i id="search" class="fas fa-search"></i> <div>
+        <div ><input id="sear"type="text" placeholder="Find Lipstick,Eyeliner,Makeup,Tutorial,etc"></div>
+
+    </div></div></div>
+
+    <div id="current">
+        <div><i id="far1" class="fas fa-trash-restore"></i></i></div>
+        <div><i id="far2" class="far fa-user"></i></div>
+   
+    </div>
+</div>
+<div id="navbar124">
+    <div id="parts1" onmouseover="navbar1()">HOME</div>
+    <div id="parts2" onmouseover="navbar2()">MAKE UP</div>
+    <div id="parts3" onmouseover="navbar3()">HAIR CARE</div>
+    <div id="parts4" onmouseover="navbar4()">SKIN CARE</div>
+    <div id="parts5" onmouseover="navbar5()">SANITIZING CARE</div>
+    <div id="parts6" onmouseover="navbar6()">COLLECTION</div>
+    <div id="parts7" onmouseover="navbar7()">REWARDS</div>
+    <div id="parts8" onmouseover="navbar8()">MYGLAMM STUDIO</div>
+    <div id="parts9" onmouseover="navbar9()">OFFERS</div>   
+</div>`
+}
+export default navbar;
---
+++ scripts/nav.js
@@ -0,0 +1,626 @@
+function navbar1(){
+    document.getElementById("parts").innerHTML=""
+    
+    document.getElementById("parts1")
+    document.getElementById("parts2")
+    document.getElementById("parts3")
+    document.getElementById("parts4")
+    document.getElementById("parts5")
+    document.getElementById("parts6")
+    document.getElementById("parts8")
+    document.getElementById("parts9")
+
+
+
+    var a =00;
+   if(a=00){
+   
+    a=0; 
+   
+   }
+}
+    var Parts = document.querySelector("#parts");
+function navbar2(){
+    document.getElementById("parts").innerHTML=""
+    document.getElementById("parts1")
+    document.getElementById("parts3")
+    document.getElementById("parts4")
+    document.getElementById("parts5")
+    document.getElementById("parts6")
+    document.getElementById("parts7")
+    document.getElementById("parts8")
+    document.getElementById("parts9")
+
+
+
+    var a = 0;
+   if(a==0){
+
+    document.getElementById("parts2")
+
+var div1=document.createElement("div")
+
+ var tag1=document.createElement("h3")
+tag1.textContent="LIPS"
+tag1.style.color="#f7007c"
+
+    var btn2 = document.createElement("div");
+    btn2.textContent="Lipstick";
+    btn2.style.border="transparent"
+
+    var btn3 = document.createElement("div");
+    btn3.textContent="Liquid Lipstick";
+   
+    var btn4 = document.createElement("div");
+    btn4.textContent="Hi-Shine Lipstick";
+   
+    
+    var btn5 = document.createElement("div");
+    btn5.textContent="Metalic Lipstick";
+   
+    
+  var btn6 = document.createElement("div");
+    btn6.textContent="Lip Gloss";
+   
+
+    
+     var btn7 = document.createElement("div");
+    btn7.textContent="lip Liner";
+    
+
+    
+     var btn8= document.createElement("div");
+    btn8.textContent="lip balm";
+   
+     var btn9 = document.createElement("div");
+    btn9.textContent="lip gliter";
+   
+     var btn10 = document.createElement("div");
+    btn10.textContent="lip stain";
+    
+
+    
+    var btn11 = document.createElement("div");
+    btn11.textContent="lip chalks";
+    
+     var btn12 = document.createElement("div");
+    btn12.textContent="lipstick set";
+   
+     var btn13 = document.createElement("div");
+    btn13.textContent="lip kit";
+    var btn14 = document.createElement("div");
+    btn14.textContent="Find your Lipstick";
+    
+
+var div2=document.createElement("div")
+
+var tag2=document.createElement("h3")
+tag2.textContent="FACE"
+tag2.style.color="#f7007c"
+
+    var btn21 = document.createElement("div");
+    btn21.textContent="Lipstick";
+
+    var btn22 = document.createElement("div");
+    btn22.textContent="Liquid Lipstick";
+   
+    var btn23 = document.createElement("div");
+    btn23.textContent="Hi-Shine Lipstick";
+   
+    
+    var btn24 = document.createElement("div");
+    btn24.textContent="Metalic Lipstick";
+   
+    
+  var btn26 = document.createElement("div");
+    btn26.textContent="Lip Gloss";
+   
+
+    
+     var btn27 = document.createElement("div");
+    btn27.textContent="lip Liner";
+    
+
+    
+     var btn28= document.createElement("div");
+    btn28.textContent="lip balm";
+   
+     var btn29 = document.createElement("div");
+    btn29.textContent="lip gliter";
+   
+     var btn30 = document.createElement("div");
+    btn30.textContent="lip stain";
+    
+
+    
+    var btn31 = document.createElement("div");
+    btn31.textContent="lip chalks";
+    
+     var btn32 = document.createElement("div");
+    btn32.textContent="lipstick set";
+   
+     var btn33 = document.createElement("div");
+    btn33.textContent="lip kit";
+    var btn34 = document.createElement("div");
+    btn34.textContent="Find your Lipstick";
+
+
+    
+var div3=document.createElement("div")
+
+var tag3=document.createElement("h3")
+tag3.textContent="EYES"
+tag3.style.color="#f7007c"
+
+    var btn41 = document.createElement("div");
+    btn41.textContent="Lipstick";
+
+    var btn42 = document.createElement("div");
+    btn42.textContent="Liquid Lipstick";
+   
+    var btn43 = document.createElement("div");
+    btn43.textContent="Hi-Shine Lipstick";
+   
+    
+    var btn44 = document.createElement("div");
+    btn44.textContent="Metalic Lipstick";
+   
+    
+  var btn46 = document.createElement("div");
+    btn46.textContent="Lip Gloss";
+   
+
+    
+     var btn47 = document.createElement("div");
+    btn47.textContent="lip Liner";
+    
+
+    
+     var btn48= document.createElement("div");
+    btn48.textContent="lip balm";
+   
+     var btn49 = document.createElement("div");
+    btn49.textContent="lip gliter";
+
+
+       
+var div4=document.createElement("div")
+
+var tag4=document.createElement("h3")
+tag4.textContent="NAILS"
+tag4.style.color="#f7007c"
+
+    var btn51 = document.createElement("div");
+    btn51.textContent="Gel";
+
+    var btn52 = document.createElement("div");
+    btn52.textContent="Matte Nail Enamel";
+   
+    var btn53 = document.createElement("div");
+    btn53.textContent="Matte Nail Polish";
+   
+    
+    var btn54 = document.createElement("div");
+    btn54.textContent="glossy Nail Enamel";
+   
+    var tag5=document.createElement("h3")
+tag5.textContent="KITS AND COMBOS";
+tag5.style.color="#f7007c"
+
+  var btn56 = document.createElement("div");
+    btn56.textContent="Makeup Kits";
+   
+
+    
+     var btn57 = document.createElement("div");
+    btn57.textContent="Bridal Makeup Kits";
+    
+    var btn58 = document.createElement("div");
+    btn58.textContent="Makeup Gift Sets";
+     
+    var tag6=document.createElement("h3")
+tag6.textContent="MAKEUP ACCESSORIES";
+tag6.style.color="#f7007c"
+
+  var btn59 = document.createElement("div");
+    btn59.textContent="Makeup Brushes";
+   
+
+    
+     var btn60 = document.createElement("div");
+    btn60.textContent="EyesShadow Brushes";
+    
+    
+   
+
+    div1.append(tag1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10,btn11,btn12,btn13,btn14);
+    div2.append(tag2, btn21, btn22, btn23, btn24, btn26, btn27, btn28, btn29, btn30,btn31,btn32,btn33,btn34);
+    div3.append(tag3, btn41, btn42, btn43, btn44, btn46, btn47, btn48, btn49)
+    div4.append(tag4,btn51, btn52, btn53, btn54,tag5, btn56, btn57, btn58,tag6, btn59,btn60)
+   parts.append(div1,div2,div3,div4)
+    a = 1;
+   }
+}
+function navbar3(){
+    document.getElementById("parts").innerHTML="";
+    document.getElementById("parts1")
+    document.getElementById("parts2")
+    document.getElementById("parts4")
+    document.getElementById("parts5")
+    document.getElementById("parts6")
+    document.getElementById("parts7")
+    document.getElementById("parts8")
+    document.getElementById("parts9")
+
+
+
+    var a = 1;
+   if(a==1){
+    document.getElementById("parts3");
+
+   var div1=document.createElement("div");
+
+    tag1=document.createElement("h3")
+    tag1.textContent="HAIR CARE"
+    tag1.style.color="#f7007c"
+
+    var btn2 = document.createElement("div");
+    // var opti1=document.createElement("option");
+    btn2.textContent="Shampoo";
+   
+    var btn3 = document.createElement("div");
+    // var op1=document.createElement("option");
+    btn3.textContent="Conditioner";
+    
+    var btn4 = document.createElement("div");
+    // var a1=document.createElement("option");
+    btn4.textContent="Hair Oil";
+    
+    
+    var btn5 = document.createElement("div");
+    // var b1=document.createElement("option");
+    btn5.textContent="Serum";
+   
+
+    
+  var btn6 = document.createElement("div");
+    // var c1=document.createElement("option");
+    btn6.textContent="Hair Mask";
+   
+     var btn7 = document.createElement("div");
+    // var d1=document.createElement("option");
+    btn7.textContent="Combos";
+
+
+//    var div2=document.getElementById("div2")
+//    var img1=document.createElement("img")
+//    img1.src="https://files.myglamm.com/site-images/400x400/Product-1.jpg"
+//    img1.style.height="250px".width="200px".margin="20px";
+
+//    var img2=document.createElement("img")
+//    img2.src="https://files.myglamm.com/site-images/400x400/Product-1.jpg"
+//    img1.style.height="250px".width="200px".margin="20px";
+
+//    var img3=document.createElement("img")
+//    img3.src="https://files.myglamm.com/site-images/400x400/Product-1.jpg"
+//    img3.style.height="250px".width="200px".margin="20px";
+
+//    var img4=document.createElement("img")
+//    img4.src="https://files.myglamm.com/site-images/400x400/Product-1.jpg"
+//    img4.style.height="250px".width="200px".margin="20px";
+
+
+  div1.append(tag1,btn2, btn3, btn4, btn5, btn6, btn7)
+//   div2.append(img1,img2,img3,img4)
+  Parts.append( div1);
+   
+    a = 2;
+   }
+
+}
+function navbar4(){
+    document.getElementById("parts").innerHTML=""
+    
+    document.getElementById("parts1")
+    document.getElementById("parts2")
+    document.getElementById("parts3")
+    document.getElementById("parts5")
+    document.getElementById("parts6")
+    document.getElementById("parts7")
+    document.getElementById("parts8")
+    document.getElementById("parts9")
+
+    
+
+
+    var a = 2;
+   if(a==2){
+
+    document.getElementById("parts4")
+   
+
+var div3=document.createElement("div")
+    var btn2 = document.createElement("div");
+    // var opti1=document.createElement("option");
+    btn2.textContent="Cleanser";
+  
+    var btn3 = document.createElement("div");
+    // var op1=document.createElement("option");
+    btn3.textContent="Toner";
+   
+    var btn4 = document.createElement("div");
+    // var a1=document.createElement("option");
+    btn4.textContent="Moisturiser";
+   
+    
+    var btn5 = document.createElement("div");
+    // var b1=document.createElement("option");
+    btn5.textContent="Serum";
+   
+    
+  var btn6 = document.createElement("div");
+    // var c1=document.createElement("option");
+    btn6.textContent="Body Lotion";
+   
+
+    
+     var btn7 = document.createElement("div");
+    // var d1=document.createElement("option");
+    btn7.textContent="Sheet Mask";
+    
+
+    
+     var btn8= document.createElement("div");
+    // var e1=document.createElement("option");
+    btn8.textContent="Lip Balms & Treatments";
+   
+     var btn9 = document.createElement("div");
+    // var f1=document.createElement("option");
+    btn9.textContent="Face Sunscreen";
+   
+     var btn10 = document.createElement("div");
+    // var g1=document.createElement("option");
+    btn10.textContent="Eye Cream";
+    
+    var btn11 = document.createElement("div");
+    // var g1=document.createElement("option");
+    btn11.textContent="Face Scrub & Exfoliators";
+    
+    div3.append( btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10,btn11)
+     Parts.append(div3);
+    a = 3;
+   }
+
+}
+
+function navbar5(){
+    document.getElementById("parts").innerHTML=""
+    
+    document.getElementById("parts1")
+    document.getElementById("parts2")
+    document.getElementById("parts3")
+    document.getElementById("parts4")
+    document.getElementById("parts6")
+    document.getElementById("parts7")
+    document.getElementById("parts8")
+    document.getElementById("parts9")
+
+
+
+    var a = 3;
+   if(a==3){
+
+    document.getElementById("parts5")
+  
+
+var div4=document.createElement("div")
+
+ var tag1=document.createElement("h3")
+tag1.textContent="  SANITIZING CARE"
+tag1.style.color="#f7007c"
+
+    var btn2 = document.createElement("div");
+    btn2.textContent="Sanitizing Wipes";
+   
+
+    var btn3 = document.createElement("div");
+    btn3.textContent="Hand Sanitizer";
+   
+    var btn4 = document.createElement("div");
+    btn4.textContent="Shower Gel";
+   
+    
+    var btn5 = document.createElement("div");
+    btn5.textContent="Body Moisturizer";
+   
+    
+  var btn6 = document.createElement("div");
+    btn6.textContent="Soaps";
+    
+     var btn7 = document.createElement("div");
+    btn7.textContent="Hand Care";
+    
+
+    
+     var btn8= document.createElement("div");
+    btn8.textContent="Sanitizing Kits";
+   
+     var btn9 = document.createElement("div");
+    btn9.textContent="Body Spray & Mist";
+   
+     var btn10 = document.createElement("div");
+    btn10.textContent="Sanitizing Spray";
+    
+
+    
+    var btn11 = document.createElement("div");
+    btn11.textContent="Fragrance;";
+    
+    div4.append(tag1,btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,btn10,btn11)
+    
+    Parts.append(div4);
+   
+    a = 4;
+   }
+
+}
+
+function navbar6(){
+    document.getElementById("parts").innerHTML=""
+    
+    document.getElementById("parts1")
+    document.getElementById("parts2")
+    document.getElementById("parts3")
+    document.getElementById("parts4")
+    document.getElementById("parts6")
+    document.getElementById("parts7")
+    document.getElementById("parts8")
+    document.getElementById("parts9")
+
+
+
+    var a = 4;
+   if(a==4){
+
+    document.getElementById("parts6")
+  
+
+var div5=document.createElement("div")
+
+ var tag1=document.createElement("h3")
+tag1.textContent="SHOP BY COLLECTION"
+tag1.style.color="#f7007c"
+
+    var btn2 = document.createElement("div");
+    btn2.textContent="MyGlamm";
+   
+
+    var btn3 = document.createElement("div");
+    btn3.textContent="SUPERFOODS";
+   
+    var btn4 = document.createElement("div");
+    btn4.textContent="Treat Love Care";
+   
+    
+     var btn5 = document.createElement("div");
+
+    btn5.textContent="Clean Beauty";
+   
+    
+  var btn6 = document.createElement("div");
+    btn6.textContent="POPxo";
+    
+     var btn7 = document.createElement("div");
+    btn7.textContent="WIPEOUT";
+    
+
+    
+     var btn8= document.createElement("div");
+    btn8.textContent="GLOW Iridescent Brightening Skincare";
+   
+     var btn9 = document.createElement("div");
+    btn9.textContent="Manish Malhotra";
+   
+     var btn10 = document.createElement("div");
+    btn10.textContent="YOUTHfull";
+    
+    var btn11 = document.createElement("div");
+    btn11.textContent="LIT Collection";
+    var btn12 = document.createElement("div");
+    btn12.textContent="POSE";
+
+    var btn13 = document.createElement("div");
+    btn13.textContent="K.Play";
+
+    var btn14 = document.createElement("div");
+    btn14.textContent="Just Dropped  ";
+    
+
+    
+    div5.append(tag1,btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,btn10,btn11,btn12,btn13,btn14)
+    
+    Parts.append(div5);
+   
+    a = 5;
+   }
+
+}
+
+function navbar7(){
+    document.getElementById("parts").innerHTML=""
+    
+    document.getElementById("parts1")
+    document.getElementById("parts2")
+    document.getElementById("parts3")
+    document.getElementById("parts4")
+    document.getElementById("parts5")
+    document.getElementById("parts6")
+    document.getElementById("parts8")
+    document.getElementById("parts9")
+
+
+
+    var a = 5;
+   if(a==5){
+   
+    a=6; 
+   
+   }
+}
+
+function navbar8(){
+    document.getElementById("parts").innerHTML=""
+    
+    document.getElementById("parts1")
+    document.getElementById("parts2")
+    document.getElementById("parts3")
+    document.getElementById("parts4")
+    document.getElementById("parts5")
+    document.getElementById("parts6")
+    document.getElementById("parts8")
+    document.getElementById("parts9")
+
+
+
+    var a = 6;
+   if(a==6){
+    document.getElementById("parts7")
+  
+
+  var div6=document.createElement("div")
+  
+   var tag1=document.createElement("h3")
+  tag1.textContent="STUDIO"
+  tag1.style.color="#f7007c"
+  
+      var btn2 = document.createElement("div");
+      btn2.textContent="BLOGSS";
+
+div6.append(tag1,btn2)
+parts.append(div6)
+    a=7; 
+   
+   }
+}
+
+function navbar9(){
+    document.getElementById("parts").innerHTML=""
+    
+    document.getElementById("parts1")
+    document.getElementById("parts2")
+    document.getElementById("parts3")
+    document.getElementById("parts4")
+    document.getElementById("parts5")
+    document.getElementById("parts6")
+    document.getElementById("parts8")
+    document.getElementById("parts9")
+
+
+
+    var a = 7;
+   if(a==7){
+   
+    a=8; 
+   
+   }
+}
