a1e6b6ac019819451325b887a864fe3af1fae9f1
nikeshkumarsingh <nikeshmasai1947@gmail.com>
2022-04-07 20:02:20 +0530

    comit

--- scripts/makeup.js
+++ scripts/makeup.js
@@ -5,24 +5,9 @@ let data1 = [
         "Image":"https://files.myglamm.com/site-images/200x200/Bridal-Beauty-Kit_2.jpg",
         "Description":"Handpicked beuty for the quintessential bride",
         "Use":"Still surfing the web to find the perfect wedding gift? Scroll no more, ‘cause 'Bridal Beauty Kit' is here! It is a beautiful white gift box that contains MyGlamm POSE HD Foundation Stick - Porcelain for a flawless second-skin finish, along with MyGlamm Chisel It Contour Kit - Face Value, a 3-in-1 – highlighter, blush and bronzer trio. To amplify your glow, this kit also has MyGlamm Magic Potion - Mermaid, the ultimate roll-on illuminator with special pearls. And lastly, it contains MyGlamm LIT Liquid Matte Lipstick - Boo for soft, smudge-proof and kissable matte lips. What else can you ask for?",
-        "benifit":`MyGlamm POSE HD Foundation Stick
+        "benifit":"MyGlamm POSE HD Foundation Stick High definition stick foundation  Blurs imperfections & conceals blemishes. Highly pigmented for intense coverage.Long-wear matte finish.   Satin texture that feels like a second skin.  Silicone microspheres smoothen & minimise the appearance of pores & fine lines. Contains shea butter, jojoba oil & vitamin E for hydration.   Free of cruelty, parabens, SLS, phthalate, alcohol & mineral oil.100% vegan.",
 
-        High definition stick foundation
-        Blurs imperfections & conceals blemishes.
-        Highly pigmented for intense coverage.
-        Long-wear matte finish. 
-        Satin texture that feels like a second skin. 
-        Silicone microspheres smoothen & minimise the appearance of pores & fine lines.
-        Contains shea butter, jojoba oil & vitamin E for hydration.  
-        Free of cruelty, parabens, SLS, phthalate, alcohol & mineral oil.
-        100% vegan.`,
-
-        "exproperty":`Shell Beige: Its a beige shade with cool undertones. 
-        Face value (Highlighter): Its a gold shade. 
-        Face value (Blush): Its a peach shade. 
-        Face value (Bronzer): Its a warm brown shade.
-        Mermaid: Its a gold shade with pink undertones. 
-        Boo: Its a rose-tinted nude shade`,
+        "exproperty":"Shell Beige: Its a beige shade with cool undertones.  Face value (Highlighter): Its a gold shade.  Face value (Blush): Its a peach shade.   Face value (Bronzer): Its a warm brown shade.Mermaid: Its a gold shade with pink undertones. Boo Its a rose-tinted nude shade",
         "Image1":"https://files.myglamm.com/site-images/800x800/Porcelain-(1)-2_1.jpg",
         "Image2":"https://files.myglamm.com/site-images/800x800/BBV-(2)_3.jpg",
         "Image3":"https://files.myglamm.com/site-images/800x800/BBV-(3)_2.jpg",
@@ -34,16 +19,9 @@ let data1 = [
         "Price": 495,
         "Image": "https://files.myglamm.com/site-images/400x400/1623081490108-modern-family-5.jpeg",
         "Description": "Hi-Wattage 3D Satin Matte Finish Lipstick With Intense Colour Payoff",
-        "use":`Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!`,
-        "benifit":`Long-lasting, soft formula
-        Nourishes the lips and fills them with moisture
-        Reduces the appearance of lines
-        Delivers extreme comfort, smooth texture 
-        Glides on in one swipe
-        Delivers extreme colour that doesnt feather, bleed or fade
-        Creamy, so your lips never feel dry`,
-        "exproperty":`Vegan and cruelty-free; Free from fragrance, parabens and D5
-        The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and you’re ready to paint the town pink, burgundy, nude or red!`,
+        "use":"Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!",
+        "benifit":"Long-lasting, soft formula Nourishes the lips and fills them with moisture Reduces the appearance of lines Delivers extreme comfort, smooth texture Glides on in one swipe Delivers extreme colour that doesnt feather, bleed or fade Creamy, so your lips never feel dry",
+        "exproperty":"Vegan and cruelty-free; Free from fragrance, parabens and D5 The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and you’re ready to paint the town pink, burgundy, nude or red!",
         "Image1":"https://files.myglamm.com/site-images/800x800/TheAffair-(2).jpg",
         "Image2":"https://files.myglamm.com/site-images/800x800/TheAffair-(3).jpg",
         "Image3":"https://files.myglamm.com/site-images/800x800/TheAffair-(4).jpg",
@@ -55,16 +33,9 @@ let data1 = [
         "Price": 395,
         "Image": "https://files.myglamm.com/site-images/400x400/01-FBO1.jpg",
         "Description": "Moringa Oil Enriched Matte Lipstick",
-        "use":`Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!`,
-        "benifit":`Long-lasting, soft formula
-        Nourishes the lips and fills them with moisture
-        Reduces the appearance of lines
-        Delivers extreme comfort, smooth texture 
-        Glides on in one swipe
-        Delivers extreme colour that doesnt feather, bleed or fade
-        Creamy, so your lips never feel dry`,
-        "exproperty":`Vegan and cruelty-free; Free from fragrance, parabens and D5
-        The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and youre ready to paint the town pink, burgundy, nude or red!`,
+        "Use":"Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!",
+        "benifit":"Long-lasting, soft formula Nourishes the lips and fills them with moisture Reduces the appearance of lines  Delivers extreme comfort, smooth texture  Glides on in one swipeDelivers extreme colour that doesnt feather, bleed or fade Creamy, so your lips never feel dry",
+        "exproperty":"Vegan and cruelty-free; Free from fragrance, parabens and D5 The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and youre ready to paint the town pink, burgundy, nude or red!",
         "Image1":"https://files.myglamm.com/site-images/800x800/Matrima-(1).jpg",
         "Image2":"https://files.myglamm.com/site-images/800x800/2-LIT-lipstic-KV-BOGO-1200x1200_25.png",
         "Image3":"https://files.myglamm.com/site-images/800x800/Matrima-(3).jpg",
@@ -76,16 +47,9 @@ let data1 = [
         "Price": 950,
         "Image": "https://files.myglamm.com/site-images/400x400/vintage-wine-7.jpeg",
         "Description": "Lightweight, Long-wear Lipsticks",
-        "use":`Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!`,
-        "benifit":`Long-lasting, soft formula
-        Nourishes the lips and fills them with moisture
-        Reduces the appearance of lines
-        Delivers extreme comfort, smooth texture 
-        Glides on in one swipe
-        Delivers extreme colour that doesnt feather, bleed or fade
-        Creamy, so your lips never feel dry`,
-        "exproperty":`Vegan and cruelty-free; Free from fragrance, parabens and D5
-        The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and youre ready to paint the town pink, burgundy, nude or red!`,
+        "Use":"Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!",
+        "benifit":"Long-lasting, soft formula  Nourishes the lips and fills them with moisture Reduces the appearance of lines  Delivers extreme comfort, smooth texture  Glides on in one swipe  Delivers extreme colour that doesnt feather, bleed or fade  Creamy, so your lips never feel dry",
+        "exproperty":"Vegan and cruelty-free; Free from fragrance, parabens and D5  The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and youre ready to paint the town pink, burgundy, nude or red!",
         "Image1":"https://files.myglamm.com/site-images/800x800/vintage-wine-1-2.jpeg",
         "Image2":"https://files.myglamm.com/site-images/800x800/vintage-wine-4.jpeg",
         "Image3":"https://files.myglamm.com/site-images/800x800/2-9.jpeg",
@@ -97,16 +61,9 @@ let data1 = [
         "Price": 699,
         "Image": "https://files.myglamm.com/site-images/400x400/SHOWGIRL.jpg",
         "Description": "Hyaluronic Acid-enriched, Moisturising, Matte Slim Lipstick",
-        "use":`Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!`,
-        "benifit":`Long-lasting, soft formula
-        Nourishes the lips and fills them with moisture
-        Reduces the appearance of lines
-        Delivers extreme comfort, smooth texture 
-        Glides on in one swipe
-        Delivers extreme colour that doesnt feather, bleed or fade
-        Creamy, so your lips never feel dry`,
-        "exproperty":`Vegan and cruelty-free; Free from fragrance, parabens and D5
-        The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and youre ready to paint the town pink, burgundy, nude or red!`,
+        "Use":"Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!",
+        "benifit":"Long-lasting, soft formula Nourishes the lips and fills them with moisture Reduces the appearance of lines Delivers extreme comfort, smooth texture  Glides on in one swipe  Delivers extreme colour that doesnt feather, bleed or fade Creamy, so your lips never feel dry",
+        "exproperty":"Vegan and cruelty-free; Free from fragrance, parabens and D5  The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and youre ready to paint the town pink, burgundy, nude or red!",
         "Image1":"https://files.myglamm.com/site-images/800x800/Goddess-(2).jpg",
         "Image2":"https://files.myglamm.com/site-images/800x800/Goddess-(4).jpg",
         "Image3":"https://files.myglamm.com/site-images/800x800/Goddess-(5).jpg",
@@ -118,16 +75,9 @@ let data1 = [
         "Price": 595,
         "Image": "https://files.myglamm.com/site-images/400x400/1623082287293-tequila-sunrise-1.jpeg",
         "Description": "Precision Perfect Matte Slimstick",
-        "use":`Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!`,
-        "benifit":`Long-lasting, soft formula
-        Nourishes the lips and fills them with moisture
-        Reduces the appearance of lines
-        Delivers extreme comfort, smooth texture 
-        Glides on in one swipe
-        Delivers extreme colour that doesnt feather, bleed or fade
-        Creamy, so your lips never feel dry`,
-        "exproperty":`Vegan and cruelty-free; Free from fragrance, parabens and D5
-        The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and youre ready to paint the town pink, burgundy, nude or red!`,
+        "Use":"Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!",
+        "benifit":"Long-lasting, soft formula  Nourishes the lips and fills them with moisture  Reduces the appearance of lines Delivers extreme comfort, smooth texture  Glides on in one swipe Delivers extreme colour that doesnt feather, bleed or fade Creamy, so your lips never feel dry",
+        "exproperty":"Vegan and cruelty-free; Free from fragrance, parabens and D5  The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and youre ready to paint the town pink, burgundy, nude or red!",
         "Image1":"https://files.myglamm.com/site-images/800x800/1645439441465-Mai-tai-with-Swatch-1.jpeg",
         "Image2":"https://files.myglamm.com/site-images/800x800/Mai-Tai-Closeup-2.jpg",
         "Image3":"https://files.myglamm.com/site-images/800x800/Close-Product-7.jpg",
@@ -139,16 +89,9 @@ let data1 = [
         "Price": 599,
         "Image": "https://files.myglamm.com/site-images/400x400/002-true-red-1.jpeg",
         "Description": "HD Long-wear Matte Lipstick",
-        "use":`Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!`,
-        "benifit":`Long-lasting, soft formula
-        Nourishes the lips and fills them with moisture
-        Reduces the appearance of lines
-        Delivers extreme comfort, smooth texture 
-        Glides on in one swipe
-        Delivers extreme colour that doesnt feather, bleed or fade
-        Creamy, so your lips never feel dry`,
-        "exproperty":`Vegan and cruelty-free; Free from fragrance, parabens and D5
-        The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and youre ready to paint the town pink, burgundy, nude or red!`,
+        "Use":"Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!",
+        "benifit":"Long-lasting, soft formula Nourishes the lips and fills them with moisture Reduces the appearance of lines Delivers extreme comfort, smooth texture   Glides on in one swipe   Delivers extreme colour that doesnt feather, bleed or fade  Creamy, so your lips never feel dry",
+        "exproperty":"Vegan and cruelty-free; Free from fragrance, parabens and D5 The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and youre ready to paint the town pink, burgundy, nude or red!",
         "Image1":"https://files.myglamm.com/site-images/800x800/TrRed-(2).jpg",
         "Image2":"https://files.myglamm.com/site-images/800x800/TrRed-(3).jpg",
         "Image3":"https://files.myglamm.com/site-images/800x800/TrRed-(4).jpg",
@@ -160,14 +103,8 @@ let data1 = [
         "Price": 545,
         "Image": "https://files.myglamm.com/site-images/400x400/1623083218517-11-pink-guava-smash-1.jpeg",
         "Description": "Pink Guava Flavoured Vitamin E-enriched Creamy Lipstick",
-        "use":`Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!`,
-        "benifit":`Long-lasting, soft formula
-        Nourishes the lips and fills them with moisture
-        Reduces the appearance of lines
-        Delivers extreme comfort, smooth texture 
-        Glides on in one swipe
-        Delivers extreme colour that doesnt feather, bleed or fade
-        Creamy, so your lips never feel dry`,
+        "Use":"Risque: Swipe on this shade of vivid magenta and make the world blush at your antics!",
+        "benifit":"Long-lasting, soft formula  Nourishes the lips and fills them with moisture  Reduces the appearance of lines Delivers extreme comfort, smooth texture  Glides on in one swipe Delivers extreme colour that doesnt feather, bleed or fade Creamy, so your lips never feel dry",
         "exproperty":`Vegan and cruelty-free; Free from fragrance, parabens and D5
         The MyGlamm Ultimatte Long Stay Matte Lipsticks offer the best of both worlds - matte and moisture. The 12 shades of pure pigmented colour will vibe with all your plans… whether it’s girls’ night or date night. Just one swipe and youre ready to paint the town pink, burgundy, nude or red!`,
         "Image1":"https://files.myglamm.com/site-images/800x800/1645440620746-Pink-Guava-(1).jpeg",
@@ -181,18 +118,12 @@ let data1 = [
         "Name":"K.PLAY FLAVOURED Compact",
         "Price":654,
         "Image":"https://files.myglamm.com/site-images/800x800/FLCMP-(1).jpg",
+        
         "description":"vanilla flavoured skin perfecting powder compact",
-        "use":`Frozen Raspberry is a bubble gum pink shade, rich in Vitamin C and antioxidants that reduce the signs of ageing like wrinkles and blemishes.
+        "Use":"Frozen Raspberry is a bubble gum pink shade, rich in Vitamin C and antioxidants that reduce the signs of ageing like wrinkles and blemishes Cheek to chic in a buff or two only with the K.Play Flavoured Blush by MyGlamm. Infused with fresh fruity flavours, this mattifying blush lasts really long and gives you that healthy, flushed look. And, don’t worry about patchiness, it’s smooth blending all the way.",
+        "benifit":"Matte finish  Intensely pigmented  Blends effortlessly",
 
-        Cheek to chic in a buff or two only with the K.Play Flavoured Blush by MyGlamm. Infused with fresh fruity flavours, this mattifying blush lasts really long and gives you that healthy, flushed look. And, don’t worry about patchiness, it’s smooth blending all the way.`,
-        "benifit":`Matte finish
-        Intensely pigmented
-        Blends effortlessly`,
-
-        "exproperty":`Crafted with spherical powders and advanced emollients for all day comfort
-        Available in fresh fruity flavours
-        Blurs imperfections and gives you healthy-looking skin
-        Cruelty-free`,
+        "exproperty":"Crafted with spherical powders and advanced emollients for all day comfort Available in fresh fruity flavours  Blurs imperfections and gives you healthy-looking skin Cruelty-free",
         "Image1":"https://files.myglamm.com/site-images/800x800/FLCMP-(1).jpg",
         "Image2":"https://files.myglamm.com/site-images/800x800/FLCMP-(2).jpg",
         "Image3":"https://files.myglamm.com/site-images/800x800/FLCMP-(3).jpg",
